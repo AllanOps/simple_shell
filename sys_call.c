c30f1dfe22acb5913a82cf4b3620f600178ef391
AllanOps <ombongiallan@gmail.com>
2022-05-18 20:29:22 +0300

    Hot fix

--- sys_call.c
+++ sys_call.c
@@ -1,7 +1,15 @@
 #include <stdio.h>                                                                                                                    
 #include <sys/types.h>                                                                                                                
 #include <unistd.h>                                                                                                                   
-                                                                                                                                      
+
+/**
+ * This code gets the PID and PPID
+ * for, yk, whatever variable pif_t does
+ *
+ * return, Always 0
+ *
+ */
+
 pid_t getpid(void);                                                                                                                   
 pid_t getppid(void);                                                                                                                  
                                                                                                                                       

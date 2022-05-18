498783745c7cab855257ca963c412bdcf4df5f49
AllanOps <ombongiallan@gmail.com>
2022-05-18 19:52:37 +0300

    First commit, exercise 0

--- sys_call.c
+++ sys_call.c
@@ -1,11 +1,15 @@
-#include <sys/types.h>
-#include <unistd.h>
-
-pid_i getpid(void);
-pid_i getppid(void);
-
-int main(){
-	printf("%d\n", getpid());
-	printf("%d\n", getppid());
-	return 0;
-}
+#include <stdio.h>                                                                                                                    
+#include <sys/types.h>                                                                                                                
+#include <unistd.h>                                                                                                                   
+                                                                                                                                      
+pid_t getpid(void);                                                                                                                   
+pid_t getppid(void);                                                                                                                  
+                                                                                                                                      
+int main(){                                                                                                                           
+                                                                                                                                      
+        printf("%d\n", getpid());                                                                                                     
+        printf("%d\n", getppid());                                                                                                    
+                                                                                                                                      
+        return 0;                                                                                                                     
+                                                                                                                                      
+}                
